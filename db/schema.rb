bb414e4eb5304ef1af323611285fd30c7f42d4ae
ericsharma <sharma.e@husky.neu.edu>
2017-05-08 11:48:43 -0400

    css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508150407) do
+ActiveRecord::Schema.define(version: 20170505155858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,6 @@ ActiveRecord::Schema.define(version: 20170508150407) do
     t.string  "youtube_id", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_songs_on_user_id", using: :btree
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "test"
   end
 
   create_table "users", force: :cascade do |t|
