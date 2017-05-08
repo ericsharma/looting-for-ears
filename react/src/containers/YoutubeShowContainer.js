c5e3eef63cb006c3aca3bf53048275372d82b0e8
ericsharma <sharma.e@husky.neu.edu>
2017-05-08 12:28:08 -0400

    fix fetch fail when deploy

--- react/src/containers/YoutubeShowContainer.js
+++ react/src/containers/YoutubeShowContainer.js
@@ -14,7 +14,8 @@ class YoutubeShowContainer extends React.Component {
 
   componentDidMount() {
     let currentUrl = window.location.href;
-    let playlistDatabaseId = currentUrl[-1];
+    console.log(currentUrl.slice(-1))
+    let playlistDatabaseId = currentUrl.slice(-1);
 
     let protoURI = "/api/v1/playlists?";
     protoURI += `id=${playlistDatabaseId}`;
